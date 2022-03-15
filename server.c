735189467ace624008a68e7db420b17232a70af1
astyles10 <alexanderstyles10@gmail.com>
2022-03-16 08:04:06 +1100

    Simple client server socket code

---
+++ server.c
@@ -0,0 +1,52 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <errno.h>
+
+int main (int argc, char* argv[]) {
+  int sockfd, newSocket, valread;
+  struct sockaddr_in address;
+  int opt = 1;
+  int addrlen = sizeof(address);
+  char buffer[1024] = {0};
+  const char *hello = "Hello from server";
+
+  if (!(sockfd = socket(AF_INET, SOCK_STREAM, 0))) {
+    printf("Failed to open socket: %s\n", strerror(errno));
+    return EXIT_FAILURE;
+  }
+
+  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+    perror("setsockopt failed: ");
+    exit(EXIT_FAILURE);
+  }
+
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(80);
+
+  if (bind(sockfd, (struct sockaddr *)&address, addrlen) < 0) {
+    perror("bind failed: ");
+    exit(EXIT_FAILURE);
+  }
+
+  if (listen(sockfd, 3) < 0) {
+    perror("Listen failed: ");
+    exit(EXIT_FAILURE);
+  }
+
+  if ((newSocket = accept(sockfd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+    perror("Accept failed: ");
+    exit(EXIT_FAILURE);
+  }
+
+  valread = read(newSocket, buffer, 1024);
+  printf("%s\n", buffer);
+  send(newSocket, hello, strlen(hello), 0);
+  printf("Hello message sent\n");
+
+  return 0;
+}
